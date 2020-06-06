7c13a1258851d29e17521bd36230c064c65b97f1
gzstyp <444141300@qq.com>
2020-06-06 13:45:13 +0800

    请求的uri

--- src/main/java/com/fwtai/security/RequestFilter.java
+++ src/main/java/com/fwtai/security/RequestFilter.java
@@ -37,6 +37,7 @@ public class RequestFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(final HttpServletRequest request,final HttpServletResponse response,final FilterChain chain) throws ServletException, IOException {
         final String uri = request.getRequestURI();
+        System.out.println("请求的uri-->"+uri);
         final String[] urls = ConfigFile.IGNORE_URLS;
         for(int x = 0; x < urls.length; x++){
             final String url = urls[x];
